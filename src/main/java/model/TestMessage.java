da87a39bb5a3bdd0d025ea98ec91b99add730bc0
Antonfilka <antonfilka@tut.by>
2021-10-27 15:01:59 +0300

    Controller added

---
+++ src/main/java/model/TestMessage.java
@@ -0,0 +1,26 @@
+package model;
+
+public class TestMessage {
+
+    private String text;
+    private String author;
+
+    public TestMessage() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
